077f1589f0791da19e90b913b16f647c6133a5ca
Eddie <ed.murillo717@gmail.com>
2026-07-18 19:33:46 -0500

    feat: initial argument checking

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+func verifyArgs(args []string) {
+	fmt.Print(args)
+}
+
 func main() {
-	fmt.Printf("TBI")
+	if len(os.Args) < 2 {
+		fmt.Println("[!] Not enough arguments")
+		return
+	}
+
+	verifyArgs(os.Args)
+
+	fmt.Println(os.Args[1:])
+
 }
